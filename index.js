8d4dd18b0718011f92053c016df29a92a35df262
Shayan NasrAbadi <williams.ca001@gmail.com>
2024-02-18 05:44:23 -0800

    Needed packages required

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const flash = require("connect-flash")
+const session = require("express-session")
+const cookieParser = require("cookie-parser")
+const passport = require("passport")
+const config = require("./configs/config.json")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(flash())
+app.use(cookieParser())
+app.use(session({secret : config.app.secret}))
+app.use(express.static(__dirname + "/public"))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.set("view engine", "ejs")
+app.set("views", __dirname + "/views")
+
+require("./utils/passport")
+
+const indexRoutes = require("./routes/index")
+app.use("/", indexRoutes)
+
+app.listen(config.app.port, () => {
+    console.log(`App is running on port ${config.app.port}`)
+})
